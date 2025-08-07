a27318060cb91f619387fc5650c54cb180b2b52f
rafael <rafael.biancatto@icloud.com>
2025-08-07 11:30:26 -0300

    entrada de dados

---
+++ exercicios de entrada de dados!!/exercicio_passarotempo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+    void main(){
+    int a = 34;
+    float b = 2.3;
+    char c = 'R';
+    char nome [6] = "RAFAEL";
+    
+    printf(" valor de a: %d", a);
+    printf("\n valor de b: %f", b);
+    printf("\n valor de c: %c", c);
+    printf("\n valor de nome: %s", nome);
+    
+    printf("\n valor p1: %c" , nome[0]);
+    printf("\n valor p2: %c" , nome[1]);
+    printf("\n valor p3: %c" , nome[2]);
+    printf("\n valor p4: %c" , nome[3]);
+    printf("\n valor p5: %c" , nome[4]);
+    printf("\n valor p6: %c" , nome[5]);
+}
