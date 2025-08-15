2d5187ed16b8014775687817321c9554da3daec9
rafael <rafael.biancatto@icloud.com>
2025-08-15 11:02:16 -0300

    estruturas de repetição

---
+++ exercicios de entrada de dados!!/exx_02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    
+    for(int i = 2; i <= 50; i++){
+    if(i % 2 == 0){
+    printf("\n%d",i);
+}
+}
+}
---
+++ exercicios de entrada de dados!!/exx_03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    int i = 0;
+    int soma = 0;
+    
+    while(i <= 100){
+         soma = soma + i;
+        // printf("%d\n", i);
+        printf("\nPos: %d, Soma: %d", i, soma);
+        i++;
+    }
+    
+    
+    
+}
